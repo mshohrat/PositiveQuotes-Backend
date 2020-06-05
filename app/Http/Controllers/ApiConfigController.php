bf716afc0def87657f878f455ab13a257bd01a35
m.shohrat <meysam.shohrat@snapp.cab>
2020-06-05 17:37:03 +0430

    add require fb token to config response

--- app/Http/Controllers/ApiConfigController.php
+++ app/Http/Controllers/ApiConfigController.php
@@ -10,6 +10,11 @@ class ApiConfigController extends Controller
     public function config(Request $request)
     {
         $user = $request->user();
-        return response()->json(['is_user_active'=>$user->is_active,'requires_token'=> $user->firebase_id == null],200);
+        $requires_token = false;
+        if($user->firebase_id == null)
+        {
+            $requires_token = true;
+        }
+        return response()->json(['is_user_active'=>$user->is_active,'requires_token'=> $requires_token],200);
     }
 }
