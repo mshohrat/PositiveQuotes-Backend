d99029f059bc9e74d110d1012e8a6824e2341f3c
m.shohrat <meysam.shohrat@snapp.cab>
2020-06-14 15:44:01 +0430

    add test for sending notification

--- app/Http/Controllers/ApiConfigController.php
+++ app/Http/Controllers/ApiConfigController.php
@@ -40,7 +40,8 @@ class ApiConfigController extends Controller
                 $dataString = json_encode($data);
 
                 $headers = [
-                    'Authorization' => 'key=AAAAkpnYfto:APA91bFu54tAtMVCVeMJpq2_XhJ6T6vXJRlOamfcYx70bkiBILO58ixJFKILeiuGmeb-6wYTlLlQGi76vBu4iLkAnbcmpns7OZ2AGZYnXD6bX0rY3q8gu6wppk0X79w5n_2j4smJh1Oj',
+//                    'Authorization' => 'key=AAAAkpnYfto:APA91bFu54tAtMVCVeMJpq2_XhJ6T6vXJRlOamfcYx70bkiBILO58ixJFKILeiuGmeb-6wYTlLlQGi76vBu4iLkAnbcmpns7OZ2AGZYnXD6bX0rY3q8gu6wppk0X79w5n_2j4smJh1Oj',
+                    'Authorization' => 'key='.env('FCM_SERVER_KEY'),
 //                    'Authorization: key=AIzaSyCjjc9HG1btRdgf_5KUY3AJHt6Rq8lOQzw',
                     'Content-Type' =>  'application/json'
 //                    'Content-Length: ' . strlen($dataString),
