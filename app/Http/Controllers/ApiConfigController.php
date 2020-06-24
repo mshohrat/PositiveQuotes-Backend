376aa75744195031d11e640c14669430eea74ff7
m.shohrat <meysam.shohrat@snapp.cab>
2020-06-24 04:31:05 +0430

    add setting to config response

--- app/Http/Controllers/ApiConfigController.php
+++ app/Http/Controllers/ApiConfigController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Utils\ResponseUtil;
+use App\UserSetting;
 use Illuminate\Http\Request;
 
 class ApiConfigController extends Controller
@@ -10,6 +11,7 @@ class ApiConfigController extends Controller
     //
     public function config(Request $request)
     {
-        return ResponseUtil::handleResponse(['is_user_active'=>$request->user()->is_active,'requires_token' =>$request->user()->firebase_id == null],ResponseUtil::SUCCESS);
+        $setting = UserSetting::where('user_id',$request->user()->id)->first();
+        return ResponseUtil::handleResponse(['is_user_active'=>$request->user()->is_active,'requires_token' =>$request->user()->firebase_id == null, 'setting' => $setting],ResponseUtil::SUCCESS);
     }
 }
