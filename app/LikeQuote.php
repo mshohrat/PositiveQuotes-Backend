b52d7447b20cf16e4182226ee1b1f0845afd1aad
m.shohrat <meysam.shohrat@snapp.cab>
2020-07-04 02:02:54 +0430

    user id and quote id changed to fillable properties in LikeQuote model

--- app/LikeQuote.php
+++ app/LikeQuote.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class LikeQuote extends Model
 {
+    protected $fillable = [
+        'user_id','quote_id'
+    ];
     //
     protected $hidden = [
         'user_id','quote_id'
